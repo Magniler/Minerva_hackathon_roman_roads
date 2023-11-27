032ec87ad6ecfc864be375c99d6d152e9c7fef2c
Magnus <201907768@post.au.dk>
2023-11-27 08:26:18 +0100

    Working comit made before major refatruring

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -2,6 +2,8 @@ import geopandas as gpd
 from pathlib import Path
 
 # Load a shape file of world lakes as a GeoDataFrame:
-path = Path('C:\\Users\\mlind\\github\\Minerva_hackathon\\data\\split segments\\road_merge_split_08112023.shp')
+path = Path(
+    "C:\\Users\\mlind\\github\\Minerva_hackathon\\data\\split segments\\road_merge_split_08112023.shp"
+)
 world_lakes = gpd.read_file(path)
 world_lakes.head(3)
